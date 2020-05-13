9d0963ec18b3812243a82f407c9064ed87f4a182
Patricia <patriciadewiratih@gmail.com>
2020-05-13 13:04:33 +0200

    s

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,8 +5,17 @@ var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x -= rotationSpeed;
-	console.log(myOtherBox.object3D.rotation.x);
+    myOtherBox.object3D.rotation.x -= rotationSpeed;
+    myOtherBox.object3D.rotation.y -= rotationSpeed; 
+    myOtherBox.object3D.rotation.z -= rotationSpeed; 
+    console.log(myOtherBox.object3D.rotation.x);
 }
+
+function inflate(){
+    myOtherBox.object3D.scale.x += rotationSpeed;
+    myOtherBox.object3D.scale.y += rotationSpeed;
+}
+
+setInterval(inflate, 16);
 
 setInterval(spin, 16); //equivalent to 60 fps
